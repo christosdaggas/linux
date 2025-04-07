0c60d517ed3b4539df7c13b9c1f30a0675d0e805
Christos A. Daggas <christos@daggas.me>
2025-04-07 20:41:07 +0300

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+set -x
 
 # -------------------------------------------------
-# Fedora 41 Setup Script (Grouped, Cleaned, Final)
+# Fedora 41 Setup Script
 # -------------------------------------------------
-#
 
 #
 # -------------------------
@@ -18,15 +19,23 @@ keepcache=True
 EOL
 
 # -------------------------
+# Change hostname
+# -------------------------
+read -p "Enter new hostname: " NEW_HOSTNAME
+echo "Changing hostname to $NEW_HOSTNAME..."
+sudo hostnamectl set-hostname "$NEW_HOSTNAME"
+sudo sed -i "s/127.0.1.1.*/127.0.1.1   $NEW_HOSTNAME/" /etc/hosts
+echo "Done! New hostname is $NEW_HOSTNAME"
+
+# -------------------------
 # Cleanup Unwanted Defaults
 # -------------------------
 sudo dnf remove -y evince rhythmbox abrt gnome-tour mediawriter
-
 sudo dnf update -y
 
 # Enable RPM Fusion repositories
-sudo dnf install -y \\
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \\
+sudo dnf install -y \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Firmware updates
@@ -50,17 +59,6 @@ sudo dnf5 install -y timeshift
 # -------------------------
 sudo dnf install -y gnome-tweaks gnome-extensions-app gnome-calendar gnome-usage
 
-# Install better Font Familys
-sudo dnf install rsms-inter-fonts
-sudo dnf copr enable atim/ubuntu-fonts
-sudo dnf install ubuntu-family-fonts
-#gsettings set org.gnome.desktop.interface font-name 'Ubuntu'
-#gsettings set org.gnome.desktop.interface document-font-name 'Ubuntu'
-#gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu'
-
-# Install Better Developer Fonts
-sudo dnf install fira-code-fonts dejavu-sans-mono-fonts jetbrains-mono-fonts
-
 # GTK4 file chooser tweak
 gsettings set org.gtk.gtk4.Settings.FileChooser sort-directories-first true
 
@@ -74,40 +72,16 @@ gsettings set org.gnome.nautilus.preferences recursive-search 'never'
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 
 # -------------------------
-# Fonts & Font Config
-# -------------------------
-sudo dnf install -y \\
-  dejavu-sans-fonts dejavu-serif-fonts dejavu-sans-mono-fonts \\
-  liberation-sans-fonts liberation-serif-fonts liberation-mono-fonts \\
-  google-noto-sans-fonts google-noto-serif-fonts google-noto-mono-fonts \\
-  fira-code-fonts 'mozilla-fira*' 'google-roboto*'
-
-# Microsoft Core Fonts
-sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-
-# Optional fontconfig tuning
-mkdir -p ~/.config/fontconfig
-cat <<EOL > ~/.config/fontconfig/fonts.conf
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-  <match target="font">
-    <edit name="hinting" mode="assign"><bool>true</bool></edit>
-    <edit name="hintstyle" mode="assign"><const>hintfull</const></edit>
-    <edit name="antialias" mode="assign"><bool>true</bool></edit>
-    <edit name="rgba" mode="assign"><const>rgb</const></edit>
-  </match>
-</fontconfig>
-EOL
-
-# -------------------------
 # Productivity Applications
 # -------------------------
-sudo dnf install -y thunderbird filezilla flatseal
+sudo dnf install -y thunderbird filezilla flatseal #vlc gimp inkscape krita
+
+# -------------------------
+# Media Applications
+# -------------------------
+# sudo dnf install -y vlc gimp inkscape krita
 
 flatpak install -y flathub com.microsoft.AzureStorageExplorer
-flatpak install -y flathub org.gnome.Secrets
-flatpak install -y flathub io.bassi.Amberol
 flatpak install -y flathub org.gnome.World.PikaBackup
 flatpak install -y flathub com.rafaelmardojai.Blanket
 flatpak install -y flathub com.github.diegoinacio.Iconic
@@ -137,12 +111,47 @@ sudo dnf install -y google-chrome-stable
 # Visual Enhancements
 # -------------------------
 sudo dnf install -y powerline-fonts
-wget -P /usr/share/fonts/ \\
-  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf \\
-  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf \\
-  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf \\
+wget -P /usr/share/fonts/ \
+  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf \
+  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf \
+  https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf \
   https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 sudo fc-cache -vf
 
+# -------------------------
+# Fonts & Font Config (Moved to End)
+# -------------------------
+
+# Enable Ubuntu fonts COPR
+sudo dnf copr enable --assumeyes atim/ubuntu-fonts
+
+# Install various fonts
+sudo dnf install -y \
+  rsms-inter-fonts \
+  ubuntu-family-fonts \
+  dejavu-sans-fonts dejavu-serif-fonts dejavu-sans-mono-fonts \
+  liberation-sans-fonts liberation-serif-fonts liberation-mono-fonts \
+  google-noto-sans-fonts google-noto-serif-fonts google-noto-mono-fonts \
+  fira-code-fonts mozilla-fira-sans-fonts google-roboto-fonts \
+  jetbrains-mono-fonts
+
+# Microsoft Core Fonts
+wget https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -O /tmp/msfonts.rpm
+sudo dnf install -y /tmp/msfonts.rpm
+
+# Optional fontconfig tuning
+mkdir -p ~/.config/fontconfig
+cat <<EOL > ~/.config/fontconfig/fonts.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+  <match target="font">
+    <edit name="hinting" mode="assign"><bool>true</bool></edit>
+    <edit name="hintstyle" mode="assign"><const>hintfull</const></edit>
+    <edit name="antialias" mode="assign"><bool>true</bool></edit>
+    <edit name="rgba" mode="assign"><const>rgb</const></edit>
+  </match>
+</fontconfig>
+EOL
 
 echo "✅ Fedora 41 setup completed. Reboot recommended."
